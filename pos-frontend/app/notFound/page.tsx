77a65017d40cb160bf024d79e44b7e5489176ef4
HammadKhan <merndeveloper786@gmail.com>
2026-03-12 03:12:44 +0500

    update changes

--- pos-frontend/app/notFound/page.tsx
+++ pos-frontend/app/notFound/page.tsx
@@ -1,23 +1,5 @@
-"use client";
+import NotFoundContent from "./NotFoundContent";
 
-import { Link } from "react-router-dom";
-import { useRouter } from "next/navigation";
-
-const NotFoundPage = () => {
-
-    const router = useRouter(); 
-
-    const handleGoHome = () => {
-        router.push("/");
-    }
-
-    return (
-        <div>
-            <h1 className="text-2xl font-bold">Not Found</h1>
-            <p className="text-sm text-gray-500">The page you are looking for does not exist.</p>
-            <button onClick={handleGoHome} className= "bg-[#27AA83] text-white p-2 rounded-md">Go to Home</button>
-        </div>
-    );
-};
-
-export default NotFoundPage;
+export default function NotFoundPage() {
+  return <NotFoundContent />;
+}
